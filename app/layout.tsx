b6b701e1ee29ba9d12d064cd7426dd956fcb1847
Hawkhub <fmwins1234@gmail.com>
2023-06-11 23:30:17 +0300

    proper favicon adding

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,11 @@ export default function RootLayout({
 }) {
   return (
     <html>
-      <head></head>
+      <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" href="/icon.png" type="image/png" sizes="192x192"/>
+        <link rel="apple-touch-icon" href="/apple-icon.png" type="image/png" sizes="180x180"/>
+      </head>
       <body>
         <Header />
         <main>
@@ -20,4 +24,4 @@ export default function RootLayout({
       </body>
     </html>
   );
-}
+};
